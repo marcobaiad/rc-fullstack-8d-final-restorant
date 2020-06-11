452781babbd44ac6311281cb7f762081afa474e6
Martin Sandander <martinfernando-sandander@hotmail.com>
2020-06-11 19:20:27 -0300

    creando paginas de rigistro de usuario e inicio de sesion basico

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import clienteAxios from './config/axios'
 //pages
 import HomePage from './pages/HomePage';
 import RegUser from './pages/RegUser';
 import LoginSession from './pages/LoginSession'
 
-
-
 function App() {
+  
   return (
    <Router>
      <Switch>
