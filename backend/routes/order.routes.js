dd3164a8c8ff94685ca141468be63b60048bc44f
andresperlo <andres_perlo@hotmail.com>
2020-06-20 13:06:48 -0300

    se crean las rutas en proceso enviado cancelar para las ordenes

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -7,13 +7,16 @@ const ControllerReadSeveralOrders = require('../controllers/order/readSeveral.or
 const ControllerReadOneOrder = require('../controllers/order/readOne.order')
 const ControllerUpdateOrder = require('../controllers/order/update.order')
 const ControllerDeleteOrder = require('../controllers/order/delete.order')
+const ControllerSendOrder = require('../controllers/order/send.order')
+const ControllerCancelOrder = require('../controllers/order/cancel.order')
 
 router.post('/', [  
-    
+     
     check('quantity', 'Cantidad vacia').notEmpty(),
     check('amountTopay', 'Cantidad a Abonar Vacio').notEmpty()
 
 ],
+
 ControllerCreateOrder.CreateOrder)
 
 router.get('/', ControllerReadSeveralOrders.seeOrders)
@@ -21,6 +24,8 @@ router.get('/', ControllerReadSeveralOrders.seeOrders)
 router.get('/:id', ControllerReadOneOrder.seeOrder)
 
 router.put('/:id', ControllerUpdateOrder.modifyOrder)
+router.put('/:id/enviar', ControllerSendOrder.sendOrder)
+router.put('/:id/cancelar', ControllerCancelOrder.cancelOrder)
 
 router.delete('/:id', ControllerDeleteOrder.DeleteOrder)
 
