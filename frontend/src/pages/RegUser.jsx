a65b9c2dac22148cb5102aa5a055887eca7dcfe9
Marco Baiad <marcobaiad@gmail.com>
2020-06-23 19:19:36 -0300

    funcionalidad en registro terminada

--- frontend/src/pages/RegUser.jsx
+++ frontend/src/pages/RegUser.jsx
@@ -1,117 +1,108 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import '../Css/Reguser.css';
 import axios from 'axios';
+import swal from 'sweetalert2';
 
 
-const RegUser = () => {
+class RegUser extends React.Component {
 
-  const [usuarios, setUsuarios] = useState([]);
-  const [ Registro, setRegistro ] = useState({ name: '', lastname: '', address: '', age: '', email: '', username: '', password: '' });
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '', 
+      lastname: '', 
+      address: '', 
+      age: '', 
+      email: '', 
+      username: '', 
+      password: '', 
+      phonenumber: '',
+      registro: []
+    };
+  }
 
-  const [ name, setName ] = useState('');
-  const [ lastname, setLastName ] = useState('');
-  const [ address, setAddress ] = useState('');
-  const [ age, setAge ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ phonenumber, setPhoneNumber ] = useState('');
   
-  const Usuarios = useCallback(async () => {
-    const res = await axios.get(`/api/v1/usuarios/`);
-        setUsuarios(res.data);
-    }, []);
-
-  useEffect(() => {
-      Usuarios();
-  }, [Usuarios]);
-
-  console.log(usuarios);
-  
-
-  const onChangeName = (e) => {
-      setName(e.target.value);
-  } 
-
-  const onChangeLastName = (e) => {
-      setLastName(e.target.value);
-  }
-
-  const onChangeAddress = (e) => {
-      setAddress(e.target.value);
-  }
-
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value);
-  }
-
-  const onChangeAge = (e) => {
-      setAge(e.target.value);
-  }
-
-  const onChangeUsername = (e) => {
-      setUsername(e.target.value);
+  handleChange = (e) => {
+    const target = e.target;
+    const value = e.target.value;
+    const name = target.name; 
+    this.setState({ [name]: value });      
   }
   
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
-  }
-
-  const onChangePhoneNumber = (e) => {
-    setPhoneNumber(e.target.value);
-  }
-
-  const onSubmitCreateHandler = async (a) => {
-    const resetForm = document.getElementById("formulario-reset");
-    a.preventDefault();
+  onSubmitCreateHandler = async (e) => {
+    let registro = this.state.registro;
+    registro.push({ name: this.state.name, lastname: this.state.lastname, address: this.state.address, age: this.state.age, username: this.state.username , password: this.state.password, phonenumber: this.state.phonenumber, email: this.state.email });
+    this.setState({ registro })
+    e.preventDefault();    
+    setInterval(e.target.reset(), 500)
     try {
-      await axios.post(`/api/v1/usuarios`, { name, lastname, address, age, username, password, email, phonenumber })
-      resetForm.reset()
+      await axios.post(`/api/v1/usuarios`, { name: this.state.name, lastname: this.state.lastname, address: this.state.address, age: this.state.age, username: this.state.username , password: this.state.password, phonenumber: this.state.phonenumber, email: this.state.email });
+      swal.fire({
+        icon: 'success',
+        title: 'Registro correcto'
+      });
     } catch (e) {
       console.log(('No se pudo crear el usuario'));
+    }    
+  }
+
+
+  OnlyNumber = (event) => {
+    if (event.charCode <= 47 ) {
+      swal.fire({
+        icon: 'error',
+        title: 'Solo puede ingresar números'
+      });
+      return false
+    } 
+    if (event.charCode >= 58) {
+      swal.fire({
+        icon: 'error',
+        title: 'Solo puede ingresar números'
+      });
+      return false
     }
   }
 
-  return (
-    <>
+  render() {
+    return(
       <div className='imgBackgroundReg registration-form'>
-        <form onSubmit={onSubmitCreateHandler} id="formulario-reset" className='container-fluid'>
+        <form onSubmit={this.onSubmitCreateHandler} id="formulario-reset" className='container-fluid'>
           <h1 className='titulo'>Registro de Usuario</h1>
           <div className="form-row">
             <div className="form-group col-md-6 mb-0">
-              <input type="text" className="form-control item" name="name" onChange={onChangeName} placeholder="Nombre" />
+              <input autoFocus type="text" className="form-control item" name="name" onChange={this.handleChange} required placeholder="Nombre" />
             </div>
             <div className="form-group col-md-6 mb-0">
-              <input type="text" className="form-control item" name="lastname" onChange={onChangeLastName} placeholder="Apellido" />
+              <input type="text" className="form-control item" name="lastname" onChange={this.handleChange} required placeholder="Apellido" />
             </div>
             <div className="form-group col-md-6 mb-0">
-              <input type="text" className="form-control item" name="username" onChange={onChangeUsername} placeholder="Usuario" />
+              <input type="text" maxLength="10" className="form-control item" onKeyPress={this.OnlyNumber} name="number" onChange={this.handleChange} required title="3810000000" placeholder="N° Celular" />
             </div>
             <div className="form-group col-md-6 mb-0">
-              <input type="password" className="form-control item" name="password" onChange={onChangePassword} placeholder="Contraseña" />
+              <input type="text" className="form-control item" onKeyPress={this.OnlyNumber} name="age" onChange={this.handleChange} required placeholder='Edad: Ej. "27"' />
             </div>
           </div>
           <div className="form-group">
-            <input type="email" className="form-control item" name="email" onChange={onChangeEmail} placeholder="Correo electrónico" />
+            <input type="email" className="form-control item" name="email" onChange={this.handleChange} title="ejemplo@mail.com" required placeholder="Correo electrónico" />
           </div>
           <div className="form-group">
-            <input type="text" className="form-control item" name="address" onChange={onChangeAddress} placeholder="Dirección" />
+            <input type="text" className="form-control item" name="address" onChange={this.handleChange} required placeholder="Dirección" />
           </div>
           <div className="form-row">
-            <div className="form-group col-md-6">
-              <input type="text" className="form-control item" name="number" onChange={onChangePhoneNumber} placeholder="N° Celular" />
+          <div className="form-group col-md-6 mb-0">
+              <input type="text" minLength="4" className="form-control item" name="username" onChange={this.handleChange} title="El usuario debe contener al menos 4 caracteres"  required placeholder="Usuario" />
             </div>
-            <div class="form-group col-md-6">
-              <input type="text" className="form-control item" name="age" onChange={onChangeAge} placeholder='Edad: Ej. "27"' />
+            <div className="form-group col-md-6 mb-0">
+              <input type="password" minLength="8" className="form-control item" name="password" onChange={this.handleChange} title="La contraseña debe tener al menos 8 caracteres y una mayuscula" required placeholder="Contraseña" />
             </div>
           </div>
           <button type="submit" className="btn btn-block create-account">Registrarse</button>
         </form>
       </div>
-    </>
-  );
+    )
+  }
 }
 
 export default RegUser;
-
