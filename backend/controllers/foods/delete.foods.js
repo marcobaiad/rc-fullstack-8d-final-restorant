7b70e07b0fd46189962f980d34a11c7826afd722
Marco Baiad <marcobaiad@gmail.com>
2020-06-25 19:53:31 -0300

    Corrección en archivos

---
+++ backend/controllers/foods/delete.foods.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const FoodsModel = require('../../models/foods.model');
+
+exports.removePlate = async (req, res) => {
+
+    try {
+
+        if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+            return res.status(404).json({mensaje: 'No hay resultado para la busqueda'});
+        }
+        
+        const plate = await FoodsModel.findByIdAndDelete(req.params.id)
+       
+        if (!plate) res.status(404).send('No hay resultados')
+
+        res.status(200).send({ mensaje: 'Se elimino el Plato de la Lista' })
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
