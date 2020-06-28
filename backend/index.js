522b71610b2a704a5bb1dfc2d0458cfea1c961b7
andresperlo <andres_perlo@hotmail.com>
2020-06-28 17:49:11 -0300

    se instala formidable y se relaciona imagen de comida

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const path = require('path');
 app.use(morgan('dev'))
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 require('./dataBase')
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 const port = process.env.PORT
 
  const Routes = require('./routes')
@@ -14,6 +18,7 @@ const port = process.env.PORT
  app.use('/api/v1', Routes)
 
 app.use(function (req, res, next) {
+   
     res.status(404).json({ mensaje: 'ERROR: 404 not found' })
 })
 
