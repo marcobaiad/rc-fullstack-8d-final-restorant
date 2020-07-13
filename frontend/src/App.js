dbfc63ad5a2c26c29d2915dce4c2323d1aef8d61
Martin Sandander <martinfernando-sandander@hotmail.com>
2020-07-12 22:11:49 -0300

    ya tiene funcionabilidad faltan detalles y estilos y grid!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../src/components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,39 +8,30 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 //pages
+import Header from '../src/components/Header';
 import HomePage from './pages/HomePage';
 import RegUser from './pages/RegUser';
 import LoginSession from './pages/LoginSession';
 import Footer from '../src/components/Footer';
-import AdmMenu from './pages/AdmMenu';
 import PrivateRoute from './components/PrivateRoute';
-import CreateFoodsPage from './pages/CreateFoodsPage'
-import EditFoods from './pages/EditFoods'
 import PlatoPage from './pages/PlatoPage';
-import GetOrderPages from './pages/GetOrderPages'
 import GetOrderUserPages from './pages/GetOrderUserPages'
-import SideBarAdm from './components/SideBarAdm'
-
-
+import MenuAdm from './components/MenuAdm'
 
 function App() {
 
 	return (
 		<Router>
-			<Route component={Header} />
+			{/* <Route component={Header} /> */}
 			<Switch>
-				<PrivateRoute path="/admin/main" role={'admin'} exact component={SideBarAdm} />
-				<PrivateRoute path="/admin/todas" role={'admin'} exact component={AdmMenu} />
-				<PrivateRoute path="/admin/allorder" role={'admin'} exact component={GetOrderPages} />
-				<PrivateRoute path="/admin/createfoods" role={'admin'} exact component={CreateFoodsPage} />
-				<PrivateRoute path="/admin/edit/:id" exact role={'admin'} component={EditFoods} />
+				<PrivateRoute path="/admin/main" role={'admin'} exact component={MenuAdm} />
 				<PrivateRoute path="/user/orders" exact role={'user'} component={GetOrderUserPages} />
 				<Route path="/plato/:id" exact component={PlatoPage} />
 				<Route path="/reg" exact component={RegUser} />
 				<Route path="/log" exact component={LoginSession} />
 				<Route exact path="/" component={HomePage} />
 			</Switch>
-			<Route component={Footer} />
+			{/* <Route component={Footer} /> */}
 		</Router>
 	);
 
---
+++ frontend/src/components/MenuAdm.jsx
@@ -0,0 +1,56 @@
+import React, { component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import $ from 'jquery';
+import '../Css/mainAdm.css'
+
+import PrivateRoute from './PrivateRoute';
+import AdmMenu from '../pages/AdmMenu';
+import CreateFoodsPage from '../pages/CreateFoodsPage'
+import GetOrderPages from '../pages/GetOrderPages'
+import EditFoods from '../pages/EditFoods'
+
+const MenuAdm = () => {
+
+  $(document).ready(function () {
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("menuDisplayed");
+    });
+  });
+
+  return (
+    <Router>
+      <div id="wrapper">
+        {/* <!-- Sidebar --> */}
+        <div id="sidebar-wrapper">
+          <ul className="sidebar-nav">
+            <li><Link to="/admin/todas">Listado de todas las comidas</Link></li>
+            <li><Link to="/admin/createfoods">Crear nuevas comidas</Link></li>
+            <li><Link to="/admin/allorder">Listado de ordenes</Link></li>
+          </ul>
+        </div>
+        {/* <!-- Page Content --> */}
+        <div id="page-content-wrapper">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-12">
+                <a href="#" className="btn btn-secondary" id="menu-toggle"><span className="glyphicon glyphicon-menu-hamburger"></span></a>
+                <h1 className="text-center">Tablero del administrador</h1>
+                <h2 className="small text-center">Asturias bar</h2>
+                <p className="text-center">En este menu de administrador podrás consultar, editar y crear nuevas comidas y tragos. También podrás ver las ordenes y sus estados.</p>
+              </div>
+            </div>
+          </div>
+          <Switch>
+            <PrivateRoute exact path="/admin/todas" component={AdmMenu} />
+            <PrivateRoute exact path="/admin/createfoods" component={CreateFoodsPage} />
+            <PrivateRoute exact path="/admin/allorder" component={GetOrderPages} />
+            <PrivateRoute path="/admin/edit/:id" exact role={'admin'} component={EditFoods} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default MenuAdm;
