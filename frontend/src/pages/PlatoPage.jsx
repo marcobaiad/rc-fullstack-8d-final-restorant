c599663ab5fafd5170133e7e7484b9d5bb491e0f
Marco Baiad <marcobaiad@gmail.com>
2020-07-13 23:42:06 -0300

    Página de plato Finalizada

--- frontend/src/pages/PlatoPage.jsx
+++ frontend/src/pages/PlatoPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import Clienteaxios from '../config/axios';
+import '../Css/platosPage.css';
 
 const PlatosPage = () => {
 
@@ -24,13 +25,14 @@ const PlatosPage = () => {
     }
 
     const plato =
-        <article className="px-0 mx-3 my-4 text-white">
-            <h3>{platoID.title}</h3>
+        <article className="ArticlePlate px-0 mx-3 mt-md-4 text-white">
+            <h3 className="mt-5">{platoID.title}</h3>
             <hr className="bg-white" />
-            <div className="row">
-                <img className="w-50 h-50" src={`http://localhost:3001` + platoID.imageUrl} alt={platoID.title} />
-                <div className="w-50 px-2">
+            <div className="row justify-content-center">
+                <img className="platoIMG col-6 px-0" src={`http://localhost:3001` + platoID.imageUrl} alt={platoID.title} />
+                <div className="col-6 px-2">
                     <p>{platoID.description}</p>
+                    <p>{platoID.summary}</p>
                 </div>
             </div>
             <button onClick={OrderHandler} className="btn btn-danger my-3">Ordenar</button>
