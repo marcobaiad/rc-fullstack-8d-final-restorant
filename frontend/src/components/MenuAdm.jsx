daf66abce1fb992fcb9dee7b3d600a5588adb8ee
Martin Sandander <martinfernando-sandander@hotmail.com>
2020-07-14 15:07:55 -0300

    falta responsive

--- frontend/src/components/MenuAdm.jsx
+++ frontend/src/components/MenuAdm.jsx
@@ -21,33 +21,32 @@ const MenuAdm = () => {
 	return (
 		<Router>
 			<div className="container-fluid" id="wrapper">
-				<div className="row">
 
-					<div className="" id="sidebar-wrapper">
+				<div className="row">
+					<div className="col-12 justify-content-center" id="sidebar-wrapper">
 						<ul className="sidebar-nav">
 							<li><Link to="/admin/todas">Listado de todas las comidas</Link></li>
 							<li><Link to="/admin/createfoods">Crear nuevas comidas</Link></li>
 							<li><Link to="/admin/allorder">Listado de ordenes</Link></li>
 						</ul>
 					</div>
+				</div>
 
-					<div className="" id="page-content-wrapper">
-						<div className="container-fluid">
-							<div className="row">
+				<div className="row" id="page-content-wrapper">
+					<div className="container-fluid">
+						<div className="row">
+							<div className="col-12">
+								<a href="#" className="btn btn-secondary" id="menu-toggle"><span className="glyphicon glyphicon-menu-hamburger"></span></a>
+								<h1 className="text-center">Tablero del administrador</h1>
+								<h2 className="small text-center">Asturias bar</h2>
+								<p className="text-center">En este menu de administrador podrás consultar, editar y crear nuevas comidas y tragos. También podrás ver las ordenes y sus estados.</p>
 
-								<div className="col-12">
-									<a href="#" className="btn btn-secondary" id="menu-toggle"><span className="glyphicon glyphicon-menu-hamburger"></span></a>
-									<h1 className="text-center">Tablero del administrador</h1>
-									<h2 className="small text-center">Asturias bar</h2>
-									<p className="text-center">En este menu de administrador podrás consultar, editar y crear nuevas comidas y tragos. También podrás ver las ordenes y sus estados.</p>
-
-									<Switch>
-										<PrivateRoute exact path="/admin/todas" component={AdmMenu} />
-										<PrivateRoute exact path="/admin/createfoods" component={CreateFoodsPage} />
-										<PrivateRoute exact path="/admin/allorder" component={GetOrderPages} />
-										<PrivateRoute path="/admin/edit/:id" exact role={'admin'} component={EditFoods} />
-									</Switch>
-								</div>
+								<Switch>
+									<PrivateRoute exact path="/admin/todas" component={AdmMenu} />
+									<PrivateRoute exact path="/admin/createfoods" component={CreateFoodsPage} />
+									<PrivateRoute exact path="/admin/allorder" component={GetOrderPages} />
+									<PrivateRoute path="/admin/edit/:id" exact role={'admin'} component={EditFoods} />
+								</Switch>
 							</div>
 						</div>
 					</div>
