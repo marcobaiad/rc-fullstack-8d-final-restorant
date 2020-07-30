eacdc8a70c4dc8187da1e21aeadc2cf567ada880
Martin <martinfernando-sandander@hotmail.com>
2020-07-30 09:04:56 -0300

    modal con leyenda sobre nosotros agregado y algunos estilos

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,170 +14,178 @@ import Restorant from '../img/CarrouselNav/Restorant.jpg'
 import ModalLogin from './ModalLogin';
 import clienteAxios from '../config/axios';
 import Logo from '../img/CarrouselNav/Logo.png';
+import SobreNosotros from './SobreNosotros'
 
 
 const Header = () => {
 
-    
+	const [isLogedIn, SetIsLogedIn] = useState(false);
+	const localToken = localStorage.getItem('token');
+	const history = useHistory();
+	const pathHome = history.location.pathname === '/';
+	const [modalShow, setModalShow] = useState(false);
+	const [sobreShow, setSobreShow] = useState(false);
 
-    const [isLogedIn, SetIsLogedIn] = useState(false);
-    const localToken = localStorage.getItem('token');
-    const history = useHistory();
-    const pathHome = history.location.pathname === '/';
-    const [modalShow, setModalShow] = useState(false);
+	const StickyNav = () => {
+		const navbar = document.getElementById("navbar");
+		const carrousel = document.getElementById("carrousel");
+		const positionNav = navbar.offsetTop;
+		if (window.pageYOffset >= positionNav) {
+			navbar.classList.add("position-fixed");
+			navbar.classList.remove("align-self-end");
+		}
+		if (carrousel) {
+			if (window.pageYOffset <= carrousel.clientHeight - (window.screen.width > 767 ? 80 : 160)) {
+				navbar.classList.remove("position-fixed");
+				navbar.classList.add("align-self-end");
+			}
+		}
+	}
 
-    const StickyNav = () => {
-        const navbar = document.getElementById("navbar");
-        const carrousel = document.getElementById("carrousel");
-        const positionNav = navbar.offsetTop;
-        if (window.pageYOffset >= positionNav) {
-            navbar.classList.add("position-fixed");
-            navbar.classList.remove("align-self-end");
-        }
-        if (carrousel) {
-            if (window.pageYOffset <= carrousel.clientHeight - (window.screen.width > 767 ? 80 : 160)) {
-                navbar.classList.remove("position-fixed");
-                navbar.classList.add("align-self-end");
-            }
-        }
-    }
+	window.onscroll = StickyNav
 
-    window.onscroll = StickyNav
+	const LogUotHandler = async () => {
+		try {
+			await clienteAxios.get(`/api/v1/usuarios/logout`,
+				{
+					headers: {
+						'Authorization': `Bearer ` + localToken
+					}
+				}
+			);
+			SetIsLogedIn(true);
+			Auth.logOut();
+			history.push(pathHome);
+		} catch (e) {
+			const { response } = e;
+			if (response.data.error & response.data.error.includes('expired')) {
+				console.log('La sesión finalizó');
+			}
+		}
+	}
 
+	const MoverContacto = () => {
+		const MenuContacto = document.getElementById("AboutUs");
+		window.scrollTo(0, MenuContacto.offsetTop)
+	}
+	const MoverMenuComida = () => {
+		const MenuComida = document.getElementById("Menu");
+		window.scrollTo(0, MenuComida.offsetTop)
+	}
+	const Timeout = () => {
+		setTimeout(MoverMenuComida, 100);
+	}
 
-    const LogUotHandler = async () => {
-        try {
-            await clienteAxios.get(`/api/v1/usuarios/logout`,
-                {
-                    headers: {
-                        'Authorization': `Bearer ` + localToken
-                    }
-                }
-            );
-            SetIsLogedIn(true);
-            Auth.logOut();
-            history.push(pathHome);
-        } catch (e) {
-            const { response } = e;
-            if (response.data.error & response.data.error.includes('expired')) {
-                console.log('La sesión finalizó');
-            }
-        }
-    }
+	const ModalSobre = () => setSobreShow(true);
 
+	const onchangeSelectHandler = (e) => {
+		// e.preventDefault()
+		if (!pathHome && e.target.value === '/') {
+			window.location.href = e.target.value
+		}
+		if (e.target.value === '#SobreNosotros') {
+			ModalSobre();
+		}
+		if (pathHome && e.target.value === '#Menu') {
+			MoverMenuComida();
+		}
+		if (!pathHome && e.target.value === '#Menu') {
+			history.push(`/${e.target.value}`);
+			Timeout();
+		}
+		if (e.target.value === '#AboutUs') {
+			MoverContacto();
+		}
+	}
 
-    const MoverContacto = () => {
-        const MenuContacto = document.getElementById("AboutUs");
-        window.scrollTo(0, MenuContacto.offsetTop)
-    }
-    const MoverMenuComida = () => {
-        const MenuComida = document.getElementById("Menu");
-        window.scrollTo(0, MenuComida.offsetTop)
-    }
-    const Timeout = () => {
-        setTimeout(MoverMenuComida, 100);
-    }
+	const roleAdmin = localStorage.getItem('role')
 
+	return (
+		<div className="d-flex flex-wrap">
+			{pathHome &&
+				<>
+					<div className="col-12 row mx-0 pr-3 pr-md-5 justify-content-end">
+						<img src={Logo} className='logo mt-5' />
+					</div>
+					<Carousel controls={false} indicators={false} id="carrousel">
+						<Carousel.Item>
+							<img
+								className="d-block w-100 h-md-100 h-lg-100 carrousel-img"
+								src={Restorant}
+								alt="First slide"
+							/>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className="d-block w-100 h-md-100 h-lg-100 carrousel-img"
+								src={Lampara}
+								alt="Third slide"
+							/>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className="d-block w-100 h-md-100 h-lg-100 carrousel-img"
+								src={Pizzas}
+								alt="Third slide"
+							/>
+						</Carousel.Item>
+					</Carousel>
+				</>
+			}
+			<div className="w-100 position-absolute align-self-end" id="navbar">
+				{roleAdmin === 'admin' ?
+					'' :
+					<Navbar variant="dark" className="px-0 py-2 mb-3 mb-lg-4 navbar-menu row flex-wrap justify-content-center justify-content-md-between m-0">
+						<Nav className="d-none d-sm-none d-md-flex d-lg-flex row pl-5 order-2 order-md-1">
+							<Navbar.Brand className="d-none d-lg-block ml-3">Asturias Food & Drinks</Navbar.Brand>
+							<Nav.Link className="text-white hover-navbar" href="/">INICIO</Nav.Link>
+							<Link className="text-white hover-navbar mt-2 mx-1" to="/" onClick={Timeout}>MENU</Link>
+							<Link className="text-white hover-navbar mt-2 mx-1" onClick={MoverContacto}>CONTACTO</Link>
+							<Link
+								className="text-white hover-navbar mt-2 mx-1"
+								to="#AboutUs" onClick={() => setSobreShow(true)}
+							>SOBRE NOSOTROS
+            	 </Link>
+							<SobreNosotros
+								show={sobreShow}
+								onHide={() => setSobreShow(false)}
+							/>
 
-    const onchangeSelectHandler = (e) => {
-        if (!pathHome && e.target.value === '/') {
-            window.location.href=e.target.value
-        }
-        if (!pathHome && e.target.value === 'SobreNosotros') {
-            history.push('/SobreNosotros');
-        }
-        if (pathHome && e.target.value === '#Menu') {            
-            MoverMenuComida();
-        }
-        if (!pathHome && e.target.value === '#Menu') {
-            history.push(`/${e.target.value}`);            
-            Timeout();
-        }
-        if (e.target.value === '#AboutUs') {
-            MoverContacto();
-        }
-    }
-
-    const roleAdmin = localStorage.getItem('role')
-
-    return (
-        <div className="d-flex flex-wrap">
-            {pathHome &&
-            <>
-                <div className="col-12 row mx-0 pr-3 pr-md-5 justify-content-end">
-                    <img src={Logo} className='logo mt-5' />
-                </div>
-                <Carousel controls={false} indicators={false} id="carrousel">
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100 h-md-100 h-lg-100 carrousel-img"
-                            src={Restorant}
-                            alt="First slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100 h-md-100 h-lg-100 carrousel-img"
-                            src={Lampara}
-                            alt="Third slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100 h-md-100 h-lg-100 carrousel-img"
-                            src={Pizzas}
-                            alt="Third slide"
-                        />
-                    </Carousel.Item>
-                </Carousel>
-            </>
-            }
-            <div className="w-100 position-absolute align-self-end" id="navbar">
-                {roleAdmin === 'admin' ?
-                    '' :
-                <Navbar variant="dark" className="px-0 py-2 mb-3 mb-lg-4 navbar-menu row flex-wrap justify-content-center justify-content-md-between m-0">
-                    <Nav className="d-none d-sm-none d-md-flex d-lg-flex row pl-5 order-2 order-md-1">
-                        <Navbar.Brand className="d-none d-lg-block ml-3">Asturias Food & Drinks</Navbar.Brand>
-                        <Nav.Link className="text-white hover-navbar" href="/">INICIO</Nav.Link>
-                        <Link className="text-white hover-navbar mt-2 mx-1" to="/" onClick={Timeout}>MENU</Link>
-                        <Link className="text-white hover-navbar mt-2 mx-1" onClick={MoverContacto}>CONTACTO</Link>
-                        <Link className="text-white hover-navbar mt-2 mx-1" to="#AboutUs">SOBRE NOSOTROS</Link>
-
-                    </Nav>
-                    <Nav className="row mx-3 order-1 order-md-2">
-                        {Auth.isAuthenticated() ?
-                            <>
-                            <Link className="text-white hover-navbar mt-2" to='/user/orders'> MIS PEDIDOS</Link>                            
-                            <Nav.Link className="text-white hover-navbar" onClick={LogUotHandler}><i className="far fa-user"></i> CERRAR SESIÓN</Nav.Link>
-                            </>
-                            :
-                            <>
-                                <Link className="text-white hover-navbar mx-2" to="/reg">REGISTRO</Link>
-                                <Link className="text-white hover-navbar mx-2" onClick={() => setModalShow(true)} id="Log-Modal"><i className="far fa-user"></i> INICIAR SESIÓN</Link>
-                                <ModalLogin
-                                    className="position-absolute"
-                                    show={modalShow}
-                                    onHide={() => setModalShow(false)}
-                                />
-                            </>
-                        }
-                    </Nav>
-                    <Form className="w-100 px-3 d-md-none mt-3 mb-0">
-                        <Form.Group controlId="exampleForm.SelectCustom">
-                            <Form.Control onChange={onchangeSelectHandler} className="drop-menu text-white" as="select" custom>
-                                <option value="" disabled selected>IR A...</option>
-                                <option value="/">INICIO</option>
-                                <option value="#Menu">MENU</option>
-                                <option value="#AboutUs">CONTACTO</option>
-                                <option value="SobreNosotros">SOBRE NOSOTROS</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Form>
-                </Navbar>
-            }
-            </div>
-        </div>
-    )
+						</Nav>
+						<Nav className="row mx-3 order-1 order-md-2">
+							{Auth.isAuthenticated() ?
+								<>
+									<Link className="text-white hover-navbar mt-2" to='/user/orders'> MIS PEDIDOS</Link>
+									<Nav.Link className="text-white hover-navbar" onClick={LogUotHandler}><i className="far fa-user"></i> CERRAR SESIÓN</Nav.Link>
+								</>
+								:
+								<>
+									<Link className="text-white hover-navbar mx-2" to="/reg">REGISTRO</Link>
+									<Link className="text-white hover-navbar mx-2" onClick={() => setModalShow(true)} id="Log-Modal"><i className="far fa-user"></i> INICIAR SESIÓN</Link>
+									<ModalLogin
+										className="position-absolute"
+										show={modalShow}
+										onHide={() => setModalShow(false)}
+									/>
+								</>
+							}
+						</Nav>
+						<Form className="w-100 px-3 d-md-none mt-3 mb-0">
+							<Form.Group controlId="exampleForm.SelectCustom">
+								<Form.Control onChange={onchangeSelectHandler} className="drop-menu text-white" as="select" custom>
+									<option value="" disabled selected>IR A...</option>
+									<option value="/">INICIO</option>
+									<option value="#Menu">MENU</option>
+									<option value="#AboutUs">CONTACTO</option>
+									<option value="#SobreNosotros">SOBRE NOSOTROS</option>
+								</Form.Control>
+							</Form.Group>
+						</Form>
+					</Navbar>
+				}
+			</div>
+		</div>
+	)
 }
 
 export default Header;
---
+++ frontend/src/components/SobreNosotros.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal'
+
+const SobreNosotros = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <h1 className='titulomodal'>Nuestra historia</h1>
+      <Modal.Body closeButton>
+        <h4 className='titulomodal pt-0'>Asturias Bar</h4>
+        <p className="text-white">
+          Durante mas de 50 años, en Asturias hemos hecho las cosas a nuestro modo. Desde el mismo comienzo, nos hemos opuesto a comprometer la calidad por tomar atajos y pensar en las ganancias antes de la excelencia. Nos mantenemos fieles a las prácticas tradicionales, respetadas desde tiempos inmemoriales y que nos fueran establecidas hace cinco generaciones: aún cortamos a mano nuestras carnes y aún utilizamos los ingredientes más exquisitos en nuestros productos. Es este tipo de compromiso el que nos ha convertido en el líder de nuestro sector y ha afianzado a Asturias como marca de confianza permanente de los consumidores.
+        </p>
+      </Modal.Body>
+      {/* <button onClick={props.onHide}>x</button> */}
+    </Modal>
+  );
+}
+
+export default SobreNosotros;
